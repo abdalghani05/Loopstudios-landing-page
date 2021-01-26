574ff69e43b1c6517b1914be75de3fc9a87ed511
abdalghani05 <anairi2005@gmail.com>
2021-01-26 13:30:03 +0100

    2eme jours

--- script.js
+++ script.js
@@ -6,13 +6,11 @@ menu.addEventListener("click",()=>{
     if(barmenu.style.display=="none"){
         barmenu.style.display="inline-block";
         menu.setAttribute("src","images/icon-close.svg")
-        nav.style.backgroundColor="#000";
         black.classList.add("black");
     }
     else{
         barmenu.style.display="none";
         menu.setAttribute("src","images/icon-hamburger.svg")
-        nav.style.backgroundColor="transparent";
         black.classList.remove("black");
     }
 })
